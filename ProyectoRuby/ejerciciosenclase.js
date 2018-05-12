011461019370bd23fa9594a23d3ca56c94ad3e6c
Angel Morales <angelmoralesb@gmail.com>
2018-05-12 10:22:47 -0500

    nodificados

---
+++ ProyectoRuby/ejerciciosenclase.js
@@ -0,0 +1,22 @@
+var obj = {
+name: 'samuel',
+age: 29
+}
+
+var ma = [1,2]
+var keys = Object.keys(obj)
+
+for (var key of keys) {
+    console.log(key);
+}
+
+var x = false;
+
+var dict = {
+    true: "verdadero", false: "falso"
+}
+console.log(dic[x]);
+
+return new Promise((resolve, reject) => {
+    
+}).then();
