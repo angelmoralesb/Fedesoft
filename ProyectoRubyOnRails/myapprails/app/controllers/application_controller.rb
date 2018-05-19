8bd7c1d583059ccf24c75677d8c826edea015475
Angel Morales <angelmoralesb@gmail.com>
2018-05-19 16:40:01 -0500

    autenticacion y login

--- ProyectoRubyOnRails/myapprails/app/controllers/application_controller.rb
+++ ProyectoRubyOnRails/myapprails/app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-    http_basic_authenticate_with name: "fatos", password: "secure_password", except: :index  protect_from_forgery with: :exception end 
- end
+    #http_basic_authenticate_with name: "fatos", password: "secure_password", except: :index  protect_from_forgery with: :exception end 
+    protect_from_forgery with: :exception
+    include SessionsHelper
+end
--- ProyectoRubyOnRails/myapprails/config/routes.rb
+++ ProyectoRubyOnRails/myapprails/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'sessions/new'
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :products
   resources :categories
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
