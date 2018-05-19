c892c9deee15050af24eee2aef6e8903ff1a51d1
Angel Morales <angelmoralesb@gmail.com>
2018-05-19 12:16:19 -0500

    Make a basic User model (including secure passwords)

--- ProyectoRubyOnRails/myapprails/db/schema.rb
+++ ProyectoRubyOnRails/myapprails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_19_000201) do
+ActiveRecord::Schema.define(version: 2019_05_18_234745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2018_05_19_000201) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["categories_id"], name: "index_products_on_categories_id"
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "products", "categories", column: "categories_id"
