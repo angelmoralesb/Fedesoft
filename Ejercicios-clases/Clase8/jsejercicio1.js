f0aad90231ae0e656a75201549744b4879c628bd
Angel Morales <angelmoralesb@gmail.com>
2018-07-08 23:43:42 -0500

    Ejercicio 1 Clase 8 DOM

---
+++ Ejercicios-clases/Clase8/jsejercicio1.js
@@ -0,0 +1,11 @@
+  window.onload = function() {
+        
+    //Número de enlaces de la página
+    //Dirección a la que enlaza el penúltimo enlace
+    //Numero de enlaces que enlazan a http://prueba
+    //Número de enlaces del tercer párrafo
+
+    //Aqui va el codigo del ejercicio
+
+  }
+  
